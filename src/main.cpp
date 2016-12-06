b1d6cbe55700d627cf7605612fc3408a5a6fa4ba
Filcer <gabfiltzer@gmail.com>
2016-12-06 02:03:13 -0600

    Menú

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,27 @@ string abrirImagen();
 
 int main()
 {
-    // TO DO...
-
-    system(abrirImagen());
+    do
+	{
+		cout << "1. Mostrar Mapa" << endl;
+    	cout << "2. Iniciar Viaje" << endl;
+		cout << "0. Salir del Programa..." << endl;
+		cin >> opcion;
+		switch(opcion)
+		{
+			case 1:
+				    system(abrirImagen());
+                    break;
+			case 2:
+				//funcion que activa el dijkstra
+                break;
+			case 0:
+				break;
+			default:
+				cout << "Opcion incorrecta." << endl;
+		}
+	} while (opcion != 0);
+   
     return 0;
 }
 
