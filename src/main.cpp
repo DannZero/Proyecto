5ff8b098db5de2653f07deee04aff4de0507781b
Daniel Ponce de León <Dann@Casa-iMac-5.local>
2016-12-06 01:41:18 -0600

    Portable a Mac abrir imagen

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,27 @@
 #include <cstdlib>
 using namespace std;
 
+string abrirImagen();
+
+
 int main()
 {
     // TO DO...
 
-    // xdg-open "imagen.jpg" //Comando de terminal para abrir imagen
-    system("xdg-open ../network_map.png");
+    system(abrirImagen());
     return 0;
+}
+
+
+string abrirImagen()
+{
+    string s = "";
+#ifdef _WIN32 // Windows (x64 and x86)
+//El de windows
+#elif __linux__
+    s = "xdg-open network_map.png"
+#elif __APPLE__
+    s = "open network_map.png"
+#endif
+    return s;
 }
